a6a10276d0befea85b432709111900293479b2c6
KevinJMLeyva <145345829+KevinJMLeyva@users.noreply.github.com>
2023-11-29 01:41:24 -0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -14,8 +14,9 @@ int main()
 {
 //Es importante cambiar las rutas de donde lee los datos.
 
-  int opc,index1,ordenado;
-  opc=0;
+  int index1,ordenado;
+  string opc;
+  opc="0";
   ordenado=0;
   std::string alumnos_Texto,nombre_archivo,nombre_archivo_matriculas,nombre_archivo_matriculas_pred;
 
@@ -36,12 +37,12 @@ if (newfile.is_open()){
       newfile.close();
    }
 
-  while(opc!=8){ ///Menú de 8 opciones, bueno 7 si no contamos el salir
+  while(opc!="8"){ ///Menú de 8 opciones, bueno 7 si no contamos el salir
     std::cout<<"\n\n_________Men"<<char(163)<<" de alumnos_________";
-    std::cout<<"\n1.Visualizar alumnos. \n2.Ordenar alumnos por orden alfabetico. \n3.A"<<char(164)<<"adir alumno. \n4.Buscar alumno. \n5.Asignar matr"<<char(161)<<"culas.\n6.Guardar cambios. \n7.Modificar alumno.  \n8.Salir.";
+    std::cout<<"\n1.Visualizar alumnos. \n2.Ordenar alumnos por orden alfabetico. \n3.A"<<char(164)<<"adir alumno. \n4.Modificar alumno, por n"<<char(163)<<" de lista. \n5.Buscar alumno por n"<<char(163)<<"mero de lista. \n6.Visualizar matr"<<char(161)<<"culas.\n7.Guardar cambios en las matr"<<char(161)<<"culas. \n8.Salir.";
     std::cout<<"\nElige una opci"<<char(162)<<"n:";
     std::cin >> opc;
-    if(opc==1){
+    if(opc=="1"){
       
       //Muestra la lista de alumnos, en el estado que se ecuentre
       if(ordenado==1){
@@ -56,13 +57,13 @@ if (newfile.is_open()){
       std::cout<<"\n";
 
     }
-    else if(opc==2){
-      //Ordena la lita
+    else if(opc=="2"){
+      //Ordena la lista
       alumnos.ordena();
       std::cout<<"\nSe han ordenado los alumnos en orden alfabetico.\n";
       ordenado=1;
     }
-    else if(opc==3){
+    else if(opc=="3"){
       //Ingresar nuevo alumno, es más rápido si lo ingresamos en la primera posición, ya que no recorremos el arreglo
       std::string nuevo_alumno;
       std::cout<<"\nIngresa el nombre del nuevo alumno:";
@@ -73,14 +74,35 @@ if (newfile.is_open()){
       std::cout<<"\nSe ha agregado a: "<<nuevo_alumno<<" y se ha ordenado la lista.\n";
       ordenado=1;
     }
-    else if(opc==4){
+     else if(opc=="4"){
+     //modificar un alumno con su número de lista, primero se debe ordenar
+      int alumno_modifico=-1;
+      string alumno_nombre;
+      alumnos.ordena();
+      std::cout<<"\nIngrese el n"<<char(163)<<"mero de lista del alumno que desea modificar:";
+      std::cin >> alumno_modifico;
+      std::cout<<"Ingrese el nuevo nombre (completo) para efectuar el cambio:";
+      std::cin.ignore(); // Eliminamos el salto de linea
+      std::getline(std::cin, alumno_nombre);
+      alumno_modifico--;
+        if(alumnos.length()>alumno_modifico and alumno_modifico>-1){
+            alumnos.set(alumno_modifico,alumno_nombre);
+            std::cout<<"Cambio realizado.";
+            ordenado=0; //Es importante marcar que la lsita se ha desoordenado, ya que algunas funciones pueden funcionar de mala manera
+        }
+        else{
+          std::cout<<"Ingrese valores razonables.";
+        }
+      
+    }
+    else if(opc=="5"){
       //Para buscar debemos ordenar la lista y depués usar métodos de la clase, ya que si no ordenamos nos dará resultados erroneos
       std::cout<<"\nOrdenando lista, para inciar busqueda...\n";
       alumnos.ordena();
       std::cout<<"\nIngresa n"<<char(163)<<"mero de lista del alumno:";
       std::cin >> index1;
       index1--;
-      if(alumnos.length()>index1 and index1>0){
+      if(alumnos.length()>index1 and index1>=0){
         std::cout<<"El alumno que se encuentra en ese n"<<char(163)<<"mero de la lista es:"<<alumnos.get(index1);
       }
       else{
@@ -88,12 +110,10 @@ if (newfile.is_open()){
       }
       ordenado=1;
     }
-    else if(opc==5){
+    else if(opc=="6"){
       //asginar las matrículas visualmente
-      if(ordenado==0){ 
-         std::cout<<"Para asginar las matr"<<char(161)<<"culas primero debes ordenar la lista de alumnos";
-      }
-      else{ 
+      //ordena a los alumnos, después muestra las matrículas
+      alumnos.ordena();
       std::cout<<"\nAsignando matr"<<char(161)<<"culas...\n";
       std::cout<<"\n";
       std::cout<<"\n";
@@ -102,18 +122,13 @@ if (newfile.is_open()){
       std::cout<<"\n";
       std::cout<<"\n";
       ordenado=1;
-      }
+
     }
-    else if(opc==6){
-      //guardar las matrículas en un archivo de texto, se puede elegir la dirección del archivo de texto
-      if(ordenado==0){ 
-        std::cout<<"No hay cambios en las matr"<<char(161)<<"culas...\n";
-        std::cout<<"\nElige una opci"<<char(162)<<"n:";
-        std::cin >> opc;
-      }
-      else{ 
-        std::string eleccion="0";
-        std::cout<<"\n"<<char(168)<<"Deseas usar la dirrec"<<char(162)<<"n predeterminadad? (Y/N)";
+    else if(opc=="7"){
+      //Ordenas los alumnos y guarda las matrículas en un archivo de texto, se puede elegir la dirección del archivo de texto
+      alumnos.ordena();
+      std::string eleccion="0";
+      std::cout<<"\n"<<char(168)<<"Deseas usar la dirrec"<<char(162)<<"n predeterminadad? (Y/N)";
         std::cin >> eleccion;
         if(eleccion=="Y"){
           alumnos.guardarMatriculas(nombre_archivo_matriculas_pred);
@@ -129,38 +144,13 @@ if (newfile.is_open()){
             alumnos.guardarMatriculas(nombre_archivo_matriculas);
             std::cout<<"Archivo guarado.\n";
           }
-        }
+        } 
         else{
           std::cout<<"\nElgie una opci"<<char(162)<<"n v"<<char(160)<<"lida";
         }
-
-      }
     }
-    else if(opc==7){
-     //modificar un alumno con su número de lista, comprobando que la lista este ordenada
-      int alumno_modifico=-1;
-      string alumno_nombre;
-      if(ordenado==1){
-      std::cout<<"\nIngrese el n"<<char(163)<<"mero de lista del alumno que desea modificar:";
-      std::cin >> alumno_modifico;
-      std::cout<<"Ingrese el nuevo nombre (completo) para efectuar el cambio:";
-      std::cin.ignore(); // Eliminamos el salto de linea
-      std::getline(std::cin, alumno_nombre);
-      alumno_modifico--;
-        if(alumnos.length()>alumno_modifico and alumno_modifico>-1){
-            alumnos.set(alumno_modifico,alumno_nombre);
-            std::cout<<"Cambio realizado.";
-            ordenado=0; //Es importante marcar que la lsita se ha desoordenado, ya que algunas funciones pueden funcionar de mala manera
-        }
-        else{
-          std::cout<<"Ingrese valores razonables.";
-        }
-      }
-      else{
-        std::cout<<"Primero, debe ordenar su lista de alumnos.";
-      }
-    }
-    else if(opc==8){
+   
+    else if(opc=="8"){
       std::cout<<"Buen d"<<char(161)<<"a.";
     }
     else{
